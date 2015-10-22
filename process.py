051c2dbeb64f7ca431bf36a125803a31542ed762
Jeremy Weinstein <jeremydw@gmail.com>
2015-10-22 14:42:43 -0700

    Commit processor.

--- process.py
+++ process.py
@@ -4,6 +4,7 @@ import sys
 sys.path.insert(0, 'lib')
 
 from PIL import Image
+from psd_tools import PSDImage
 import os
 import yaml
 
@@ -15,7 +16,36 @@ OUT_DIR = CONFIG['out_dir']
 
 
 def process(rule):
-  pass
+  for fmt in rule['formats']:
+    size = rule['size']
+    path = os.path.join(OUT_DIR, rule['name'] + '.' + fmt.lower())
+    if INPUT_PATH.endswith('psd'):
+      psd = PSDImage.load(INPUT_PATH)
+      image = psd.as_PIL()
+    else:
+      image = Image.open(INPUT_PATH)
+    image.thumbnail(size, Image.ANTIALIAS)
+    background_color = rule.get('background', (255, 255, 255, 1))
+    if background_color:
+      if rule.get('trim') not in (None, False):
+        size = image.size
+      else:
+        size = size
+      background = Image.new('RGBA', size, tuple(background_color))
+      offset = (max((size[0] - image.size[0]) / 2, 0), max((size[1] - image.size[1]) / 2, 0))
+      image.load()  # Required for split.
+      if INPUT_PATH.endswith('png'):
+        background.paste(image, offset, mask=image.split()[3])
+      else:
+        background.paste(image, offset)
+      image = background
+    image.save(
+        path,
+        fmt,
+        quality=rule.get('quality', 100),
+        optimize=True,
+        progressive=True)
+    print 'Saved: {}'.format(path)
 
 
 def main():
