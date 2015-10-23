05ef58ca25c581f8aff74219eeea1f217549d361
Jeremy Weinstein <jeremydw@gmail.com>
2015-10-22 22:53:23 -0700

    Code cleanup.

--- process.py
+++ process.py
@@ -15,29 +15,39 @@ INPUT_PATH = CONFIG['input']
 OUT_DIR = CONFIG['out_dir']
 
 
+def _get_image():
+  if INPUT_PATH.endswith('psd'):
+    psd = PSDImage.load(INPUT_PATH)
+    return psd.as_PIL()
+  else:
+    return Image.open(INPUT_PATH)
+
+
+def _get_size(image, rule):
+  if rule.get('trim') not in (None, False):
+    return image.size
+  return rule['size']
+
+
+def _get_offset(image, size):
+  return (max((size[0] - image.size[0]) / 2, 0),
+          max((size[1] - image.size[1]) / 2, 0))
+
+
 def process(rule):
   for fmt in rule['formats']:
     size = rule['size']
     path = os.path.join(OUT_DIR, rule['name'] + '.' + fmt.lower())
-    if INPUT_PATH.endswith('psd'):
-      psd = PSDImage.load(INPUT_PATH)
-      image = psd.as_PIL()
-    else:
-      image = Image.open(INPUT_PATH)
+    image = _get_image()
     image.thumbnail(size, Image.ANTIALIAS)
+    size = _get_size(image, rule)
+    offset = _get_offset(image, size)
     background_color = rule.get('background', (255, 255, 255, 1))
-    if background_color:
-      if rule.get('trim') not in (None, False):
-        size = image.size
-      else:
-        size = size
-      background = Image.new('RGBA', size, tuple(background_color))
-      offset = (max((size[0] - image.size[0]) / 2, 0), max((size[1] - image.size[1]) / 2, 0))
-      image.load()  # Required for split.
-      mask = image.split()[-1]
-      background.paste(image, offset, mask=mask)
-      image = background
-    image.save(
+    background = Image.new('RGBA', size, tuple(background_color))
+    image.load()  # Required for split.
+    mask = image.split()[-1]
+    background.paste(image, offset, mask=mask)
+    background.save(
         path,
         fmt,
         quality=rule.get('quality', 100),
